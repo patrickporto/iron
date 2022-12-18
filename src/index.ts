3b0693036c3fc1be85439d0756d73f0f563b1c3c
Patrick Porto <patrick.porto@loft.com.br>
2022-12-17 20:31:45 -0700

    feat: add link module or system

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import chalk from "chalk";
+import fs from "fs";
 import { program } from "commander";
+import path from "path";
 
 const IRON_VERSION = "0.0.1";
 
@@ -8,12 +10,25 @@ program
     .version(IRON_VERSION)
     .description(
         "A powered CLI to improve developer experience for Foundry modules and systems."
-    )
-    .option("-p, --peppers", "Add peppers")
-    .option("-P, --pineapple", "Add pineapple")
-    .option("-b, --bbq", "Add bbq sauce")
-    .option("-c, --cheese <type>", "Add the specified type of cheese [marble]")
-    .option("-C, --no-cheese", "You do not want any cheese")
-    .parse(process.argv);
+    );
+
+program.command("link")
+    .description("Link a module or system to Foundry VTT")
+    .action(() => {
+        let ironConfig
+        try {
+            ironConfig = JSON.parse(fs.readFileSync("./ironconfig.json", "utf-8"))
+        } catch(e) {
+            console.log(chalk.red("Could not find ironconfig.json in the current directory."))
+            return
+        }
+        console.log(chalk.green(`Linking ${ironConfig.type}...`));
+        const targetPath = `${ironConfig.foundryData}${path.sep}Data${path.sep}${ironConfig.type}s${path.sep}${ironConfig.canonicalName}`;
+        fs.symlinkSync(path.resolve(ironConfig.rootPath), targetPath, "dir");
+        console.log(chalk.green(`Linked ${ironConfig.type}!`));
+    });
+
+
+program.parse(process.argv);
 
 
